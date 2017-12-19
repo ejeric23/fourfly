296c3b5265e76ec25d7a2c7c10114f3372f12ceb
EC2 Default User <ec2-user@ip-172-31-7-124.us-east-2.compute.internal>
2017-12-19 02:25:14 +0000

    Add navbar and profiles views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :devise_parameters_whitelist, if: :devise_controller?
+
+  protected
+
+  def devise_parameters_whitelist
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username, :bio, :location])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username, :bio, :location])
+  end
 end
